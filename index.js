a351c60b2ce8dd3e2c5eb2729f43e6760aa7c2f6
manbeltran <jbelt005@ucr.edu>
2022-07-13 21:59:55 -0700

    complete

---
+++ index.js
@@ -0,0 +1,11 @@
+/* 
+
+JS PLAN:
+
+JS to place marker when selects a square, but can't over ride or switch already selected squares
+Check for Win/Draw
+Switch Turns
+
+*/
+
+const boxEvent = document.querySelectorAll('div.grid.box')
