7ece2bc52f2d79659495140b967fcd4c7b6ef2b6
manbeltran <jbelt005@ucr.edu>
2022-07-17 21:16:31 -0700

    updated css and start of java

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-/* 
 
-JS PLAN:
 
-JS to place marker when selects a square, but can't over ride or switch already selected squares
-Check for Win/Draw
-Switch Turns
+let boxes = document.querySelectorAll('.box')
 
+boxes = Array.from(boxes)
+
+let player1 = 'X'
+
+/*
+let winningCombinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
 */
+boxes.forEach(function(box){
+    box.addEventListener('click',function(){
+        if(box.innerText.trim() != '') return //if box is NOT null, return and dont do anything
+        box.innerText = player1
+        player1 = player1 == 'X' ? 'O' : 'X' //if current player is already X, insert O, else place an X
+    })
+})
 
-const boxEvent = document.querySelectorAll('div.grid.box')
+
+//function to check for winner
+
+
+//refresh page
+
+document.getElementById('restart').addEventListener('click', function(){
+    location.reload();
+    return false;
+})
+
